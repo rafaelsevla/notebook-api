a160cb87df20349ac0e361b61e2919552fc91acb
Rafitos <rafaelsevla@gmail.com>
2025-12-29 16:13:44 -0300

    override as_json to include kind_description on contacts list

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,19 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+
+  def author
+    "Rafitos"
+  end
+
+  def kind_description
+    self.kind.description
+  end
+
+  def as_json(options = {})
+    super(
+      root: true,
+      methods: [ :kind_description, :author ],
+      include: { kind: { only: :description } }
+    )
+  end
 end
