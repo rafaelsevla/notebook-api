7c65d973ee4e3e72c0f4f7f136d159ab8bec369a
Rafitos <rafaelsevla@gmail.com>
2025-12-30 00:33:59 -0300

    update address definitions

---
+++ db/migrate/20251230032710_change_contact_id_in_addresses.rb
@@ -0,0 +1,5 @@
+class ChangeContactIdInAddresses < ActiveRecord::Migration[8.0]
+  def change
+    change_column_null :addresses, :contact_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_30_025539) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_30_032710) do
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "city"
-    t.integer "contact_id", null: false
+    t.integer "contact_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["contact_id"], name: "index_addresses_on_contact_id"
