766455cef979fcce0ebec3dcacb4a40e9dfa6dc5
Rafitos <rafaelsevla@gmail.com>
2026-01-20 21:17:39 -0300

    add missing fields to response in contact and kind

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
   def attributes(*args)
     hash = super(*args)
     hash[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
