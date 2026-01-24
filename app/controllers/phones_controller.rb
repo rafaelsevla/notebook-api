c21fa3997114fc2b81535d83fd19d11d025b4847
Rafitos <rafaelsevla@gmail.com>
2026-01-24 14:02:54 -0300

    add complete crud to phones

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,14 +1,45 @@
 class PhonesController < ApplicationController
-  before_action :set_phones, only: %i[ show ]
+before_action :set_contact
 
-  # GET /phones/1
+  # GET /contacts/1/phones
   def show
-    render json: @phones
+    render json: @contact.phones
+  end
+
+  # POST /contacts/1/phone
+  def create
+    @contact.phones << Phone.new(phone_params)
+    if @contact.save
+      render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+    else
+      render json: @contact.errors, status: :unprocessable_content
+    end
+  end
+
+  # PATCH/PUT /contacts/1/phone
+  def update
+    phone = Phone.find(phone_params[:id])
+
+    if phone.update(phone_params)
+      render json: @contact.phones
+    else
+      render json: @contact.errors, status: :unprocessable_content
+    end
+  end
+
+  # DELETE /contacts/1/address
+  def destroy
+    phone = Phone.find(phone_params[:id])
+    phone.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_phones
-      @phones = Contact.find(params[:contact_id]).phones
+    def set_contact
+      @contact = Contact.find(params[:contact_id])
+    end
+
+    def phone_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
