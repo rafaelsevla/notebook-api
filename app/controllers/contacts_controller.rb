7208801364d2a900c9f008d09c970e4c0ee14604
Rafitos <rafaelsevla@gmail.com>
2026-01-24 02:23:04 -0300

    Deserialization

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -47,11 +47,7 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(
-        :name, :email, :birthdate, :kind_id,
-        phones_attributes: [ :id, :number, :_destroy ],
-        address_attributes: [ :id, :street, :city ]
-      )
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 
     def record_not_found
