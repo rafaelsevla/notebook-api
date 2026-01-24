68cc08829dc80ebe9695619b46f94ab67373bfe6
Rafitos <rafaelsevla@gmail.com>
2026-01-24 15:49:50 -0300

    add time expiration

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,7 +1,7 @@
 class AuthsController < ApplicationController
   def create
     hmac_secret = "my$ecretK3y"
-    payload = { name: params[:name], exp: Time.now.to_i + 10 }
+    payload = { name: params[:name], exp: Time.now.to_i + 60 }
     token = JWT.encode(payload, hmac_secret, "HS256")
 
     render json: { 'token': token  }
