3419a43883ea6a68da70d634d115622f3a74d6e5
Rafitos <rafaelsevla@gmail.com>
2026-01-27 22:49:40 -0300

    override hash to access json to tests

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+class Hash
+  def json(parts)
+    ary = parts.split(">")
+    ary.reduce(self) do |memo, key|
+      memo.fetch(key.to_s.strip) if memo
+    end
+  end
+end
+
 describe V1::ContactsController, type: :controller do
   it 'request index and return 200 OK' do
     request.accept = 'application/json'
@@ -27,7 +36,8 @@ describe V1::ContactsController, type: :controller do
 
     response_body = JSON.parse(response.body)
 
-    expect(response_body.fetch('data').fetch('id')).to eq(contact.id.to_s)
-    expect(response_body.fetch('data').fetch('type')).to eq('contacts')
+    expect(response_body.json('data > id')).to eq(contact.id.to_s)
+    expect(response_body.json('data > type')).to eq('contacts')
+    expect(response_body.json('data > attributes > name')).to eq(contact.name)
   end
 end
