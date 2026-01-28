ae2e0259fc399367158b76acc7d65f955f07d2d9
Rafitos <rafaelsevla@gmail.com>
2026-01-27 22:27:05 -0300

    remove deprecated param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::API
 
   def ensure_json_request
     return if request.headers["Accept"] =~ /json/
-    render nothing: true, status: 406
+    render body: nil, status: 406
   end
 end
